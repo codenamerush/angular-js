6ca492214b5bcf3b586233a8241663314fe8071b
Aarushi <aarushisharda@gmail.com>
2018-05-15 18:49:11 +0530

    Updated folders

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('MenuCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
